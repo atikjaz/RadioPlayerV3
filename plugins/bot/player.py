cb9afa3d7cedec867e70263052e36b1d6e019da1
atikjaz <64772699+atikjaz@users.noreply.github.com>
2021-09-01 21:33:26 +0530

    Update player.py

--- plugins/bot/player.py
+++ plugins/bot/player.py
@@ -570,12 +570,12 @@ allcmd = ["play", "current", "playlist", "song", f"song@{USERNAME}", f"play@{USE
 async def not_chat(_, m: Message):
     buttons = [
             [
-                InlineKeyboardButton("CHANNEL", url="https://t.me/AsmSafone"),
-                InlineKeyboardButton("SUPPORT", url="https://t.me/SafoTheBot"),
+                InlineKeyboardButton("CHANNEL", url="https://t.me/joinchat/hg4dqagMTsE4MDFl"),
+                InlineKeyboardButton("SUPPORT", url="https://t.me/MovieAndWeb_Discuss"),
             ],
             [
-                InlineKeyboardButton("🤖 MAKE YOUR OWN BOT 🤖", url="https://heroku.com/deploy?template=https://github.com/AsmSafone/RadioPlayerV3"),
+                InlineKeyboardButton("BACKUP", url="https://t.me/GodFather_Community"),
             ]
          ]
-    k=await m.reply_photo(photo="https://telegra.ph/file/4e839766d45935998e9c6.jpg", caption="**Sorry, You Can't Use This Bot In This Group! 🤷‍♂️ But You Can Make Your Own Bot Like This From The [Source Code](https://github.com/AsmSafone/RadioPlayerV3) Below 😉!**", reply_markup=InlineKeyboardMarkup(buttons))
+    k=await m.reply_photo(photo="https://telegra.ph/file/f1dbf86548edd1d1102ea.jpg", caption="**Sorry, You Can't Use This Bot In This Group! 🤷‍♂️ But You Can Make Your Own Bot Like This!**", reply_markup=InlineKeyboardMarkup(buttons))
     await mp.delete(m)
